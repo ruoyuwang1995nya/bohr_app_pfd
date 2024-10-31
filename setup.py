01c80b7a4a6094a85290306835a31239c4b08a0e
ruoyu wang <ruoyuwang1995@gmail.com>
2024-10-31 16:43:40 +0800

    fix: update dpgen2 version

--- setup.py
+++ setup.py
@@ -21,7 +21,8 @@ setuptools.setup(
         "seekpath",
         "fpop>=0.0.7",
         "boto3",
-        "pfd-kit @ git+https://github.com/ruoyuwang1995nya/pfd-kit.git@v0.1.0#eqq=pfd-kit"
+        "pfd-kit @ git+https://github.com/ruoyuwang1995nya/pfd-kit.git@v0.1.0#eqq=pfd-kit",
+        "dpgen2 @ git+https://github.com/ruoyuwang1995nya/dpgen2.git"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
